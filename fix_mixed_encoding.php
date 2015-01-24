f690492599682ad91d9428c227192cd6dca31551
st <st@ptb13.cabnet.cabag.ch>
2015-01-24 11:09:15 +0100

    Added license and author information

--- fix_mixed_encoding.php
+++ fix_mixed_encoding.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @author Jonas Felix <jf@cabag.ch>
+ * @author Tizian Schmidlin <st@cabag.ch>
+ * @packag fix_mixed_encoding
+ */
+/**
+ * This script was originally cloned form https://github.com/CDRO/fixMixedEncoding.git
+ */
 // show usage
 if(!is_file($argv[1]) || empty($argv[0])) {
 	echo "\nUsage: > php fix_mixed_encoding.php inputfile.sql outputfile.sql\n";
